2b3a47ca580c9491b8d688dcb9b253e0febc725c
Mario-utn-2021 <mperezquiroga@frba.utn.edu.ar>
2021-08-06 10:27:39 -0300

    routiando paginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { InformacionComponent } from './pages/informacion/informacion.component';
+import { QuienesSomosComponent } from './pages/quienes-somos/quienes-somos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'quienessomos', component: QuienesSomosComponent},
+  { path: 'informacion', component: InformacionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
