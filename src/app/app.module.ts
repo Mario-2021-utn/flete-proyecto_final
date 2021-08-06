c99549252b0e285860e04f6b0f637da84ba61cd5
Mario-utn-2021 <mperezquiroga@frba.utn.edu.ar>
2021-08-06 10:28:43 -0300

    cargando las paginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TitleComponent } from './components/title/title.component';
+import { QuienesSomosComponent } from './pages/quienes-somos/quienes-somos.component';
+import { InformacionComponent } from './pages/informacion/informacion.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TitleComponent
+    TitleComponent,
+    QuienesSomosComponent,
+    InformacionComponent,
+    HomeComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
