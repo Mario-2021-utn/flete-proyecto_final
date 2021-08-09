52c100b17aa60feafcff1b624f360c4e1b4e6399
Mario-utn-2021 <mperezquiroga@frba.utn.edu.ar>
2021-08-09 12:17:40 -0300

    cambiando el link del servicio a https

--- src/app/services/clima.service.ts
+++ src/app/services/clima.service.ts
@@ -10,7 +10,7 @@ export class ClimaService {
     private http: HttpClient
   ) { }
 
-  fetchUrl= "http://api.openweathermap.org/data/2.5/weather?id=524901&appid=01167dc0e11d0746543c88622fcc07dc&q=buenos+aires&units=metric&lang=es "
+  fetchUrl= "https://api.openweathermap.org/data/2.5/weather?id=524901&appid=01167dc0e11d0746543c88622fcc07dc&q=buenos+aires&units=metric&lang=es "
 
   getClima(){
     return this.http.get<any>(this.fetchUrl);
